d3ff817ff2d1c8cf2b5aa13fd521317ab9038dee
Sébastien Berthier <sebastien.berthier@e.ujf-grenoble.fr>
2014-11-06 13:17:25 +0100

    mrege conflict client lourd

--- HomeCinema-client/src/main/HomeCinemaClient.java
+++ HomeCinema-client/src/main/HomeCinemaClient.java
@@ -5,6 +5,11 @@
  */
 package main;
 
+import main.utils.Manager_if;
+import main.utils.ModuleManager;
+import main.utils.Menu;
+import main.utils.ExitManager;
+import managers.loggin.LogginManager;
 import dtos.FilmDto;
 import dtos.VideoDto;
 import ejbs.ManageFilmRemote;
@@ -15,7 +20,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Properties;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.InitialContext;
@@ -24,71 +32,40 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import main.utils.ModuleMenuManager;
+import main.utils.ReturnManager.ReturnMenuException;
+import managers.films.FilmManager;
+import managers.users.UserManager;
+import models.LoggedModel;
 import org.json.JSONException;
 
 /**
  *
  * @author titou
  */
-public class HomeCinemaClient extends JFrame implements ActionListener {
+public class HomeCinemaClient extends ModuleMenuManager {
 
-    public JTextField inp_id;
-    public JTextField inp_price;
-    public JTextField inp_trailer;
-    public JTextField inp_video;
-    public JButton send;
-    public AdminFilm adFilm;
+  public static void main(String[] args) {
+    System.out.println("Application d'administration ++");
+    HomeCinemaClient client = new HomeCinemaClient();
+    client.runMenu();
+  }
 
-    public static void main(String[] args) {
+  public HomeCinemaClient() {
+    super(new LoggedModel());
+    menu = new Menu(this,log, "Main menu:");
+  }
 
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new HomeCinemaClient().launch();
-            }
-        });
+  public void runMenu() {
+    // TODO: runMenuConsole + runMenuGraphique
+    try {
+    menu.addManager((Manager_if) new ExitManager(log));
+    menu.addManager((Manager_if) new FilmManager(log));
+    menu.addManager((Manager_if) new UserManager(log));
+    //   menu.addManager((Manager_if) new LogginManager(log));
+      menu.runMenuConsole();
+    } catch (ReturnMenuException e) {
+      System.out.println("bye");
     }
-
-    public void launch() {
-        adFilm = new AdminFilm();
-        makeJframe();
-        setVisible(true);
-    }
-
-    public void makeJframe() {
-        setTitle("Admin");
-        setSize(600, 600);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(0, 2));
-        this.add(new JLabel("id"));
-        inp_id = new JTextField(20);
-        this.add(inp_id);
-        this.add(new JLabel("prix"));
-        inp_price = new JTextField(20);
-        this.add(inp_price);
-        this.add(new JLabel("trailer"));
-        inp_trailer = new JTextField(100);
-        this.add(inp_trailer);
-        this.add(new JLabel("video"));
-        inp_video = new JTextField(100);
-        this.add(inp_video);
-        send = new JButton("Envoyer");
-        send.addActionListener(this);
-        this.add(send);
-
-    }
-
-    public void actionPerformed(ActionEvent ae) {
-        try {
-            adFilm.makeAndSendMovie(new Long(inp_id.getText()), inp_video.getText(), inp_trailer.getText(), new Integer(inp_price.getText()));
-        } catch (JSONException ex) {
-            Logger.getLogger(HomeCinemaClient.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(HomeCinemaClient.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParseException ex) {
-            Logger.getLogger(HomeCinemaClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
+  }
 }
