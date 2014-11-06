0322c77ed4a78d1e159ea7cca97f43b445ed0619
robin <bonjour1992@laposte.net>
2014-11-06 14:50:30 +0100

    ajout mergeOrSave product video

--- HomeCinema-ejb/src/java/managers/dtos/ProductDtoManager.java
+++ HomeCinema-ejb/src/java/managers/dtos/ProductDtoManager.java
@@ -10,40 +10,52 @@ import dtos.ProductDto;
 import entities.Film;
 import entities.Product;
 import java.util.Date;
+import javax.persistence.EntityManager;
 
 /**
  *
  * @author titou
  */
-public  class ProductDtoManager {
-    
-    public static ProductDto getDto(Product p)
-    {
-        if (p == null)
+public class ProductDtoManager {
+
+    public static ProductDto getDto(Product p) {
+        if (p == null) {
             return null;
-        
+        }
+
         ProductDto pdto = new ProductDto();
         pdto.id = p.getId();
-	pdto.addDate = p.getAddDate();
-	pdto.name = p.getName();
-	pdto.nbSales = p.getNbSales();
-	pdto.price = p.getPrice();
-	pdto.state = p.getState();
+        pdto.addDate = p.getAddDate();
+        pdto.name = p.getName();
+        pdto.nbSales = p.getNbSales();
+        pdto.price = p.getPrice();
+        pdto.state = p.getState();
 
         return pdto;
     }
-    
-    public static Product makeProduct(ProductDto pdto)
-    {
+
+    public static Product makeProduct(ProductDto pdto) {
         Product p = new Product();
-	p.setId(pdto.id);
-	p.setAddDate(pdto.addDate);
-	p.setName(pdto.name);
-	p.setNbSales(pdto.nbSales);
-	p.setPrice(pdto.price);
-	p.setState(pdto.state);
+        p.setId(pdto.id);
+        p.setAddDate(pdto.addDate);
+        p.setName(pdto.name);
+        p.setNbSales(pdto.nbSales);
+        p.setPrice(pdto.price);
+        p.setState(pdto.state);
 
         return p;
     }
-        
+
+    public static Product mergeOrSave(ProductDto pdto, EntityManager em) {
+        Product p = em.find(Product.class, pdto.id);
+        if (p == null) {
+            p = makeProduct(pdto);
+            em.persist(p);
+        } else {
+            p.setName(pdto.name);
+            p.setPrice(pdto.price);
+            p.setState(pdto.state);
+        }
+        return p;
+    }
 }
--- HomeCinema-ejb/src/java/managers/dtos/VideoDtoManager.java
+++ HomeCinema-ejb/src/java/managers/dtos/VideoDtoManager.java
@@ -7,27 +7,39 @@ package managers.dtos;
 
 import dtos.VideoDto;
 import entities.Video;
+import javax.persistence.EntityManager;
 
 /**
  *
  * @author titou
  */
 public class VideoDtoManager {
-    public static VideoDto getDto (Video v)
-    {
+
+    public static VideoDto getDto(Video v) {
         VideoDto vdto = new VideoDto();
-	vdto.id = v.getId();
-        vdto.resolution=v.getResolution();
-        vdto.url=v.getUrl();
+        vdto.id = v.getId();
+        vdto.resolution = v.getResolution();
+        vdto.url = v.getUrl();
         return vdto;
     }
-    
-    public static Video makeVideo (VideoDto vdto)
-    {
+
+    public static Video makeVideo(VideoDto vdto) {
         Video v = new Video();
-	v.setId(vdto.id);
+        v.setId(vdto.id);
         v.setResolution(vdto.resolution);
         v.setUrl(vdto.url);
+        return v;
+    }
+
+    public static Video mergeOrSave(VideoDto vdto, EntityManager em) {
+        Video v = em.find(Video.class, vdto.id);
+        if (v == null) {
+            v = makeVideo(vdto);
+            em.persist(v);
+        } else {
+            v.setResolution(vdto.resolution);
+            v.setUrl(vdto.url);
+        }
         return v;
     }
 }
