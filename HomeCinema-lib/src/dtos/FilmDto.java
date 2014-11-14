5a46b1d6a3350659ead3e0bca52cce7b81f51a13
narcisse <narcisse@narcisse>
2014-11-14 15:01:33 +0100

    affiche film new version (css)

--- HomeCinema-lib/src/dtos/FilmDto.java
+++ HomeCinema-lib/src/dtos/FilmDto.java
@@ -17,6 +17,70 @@ public class FilmDto implements Serializable {
     public String title;
     public String overview;
     public String cover;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Integer getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(Integer runtime) {
+        this.runtime = runtime;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
     public Date release_date;
     public String country;
     public Integer runtime;
