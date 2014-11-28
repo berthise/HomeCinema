9a25d4502666e64c843338e172f62b439dd89a21
robin <bonjour1992@laposte.net>
2014-11-28 11:42:43 +0100

    optimisation recherche

--- HomeCinema-ejb/src/java/ejbs/admin/ManageProduct.java
+++ HomeCinema-ejb/src/java/ejbs/admin/ManageProduct.java
@@ -128,54 +128,67 @@ public class ManageProduct implements ManageProductRemote {
 	return ProductDtoManager.getDto(ProductDtoManager.mergeOrSave(pdto, em));
     }
 
-    private List<Product> findProducts(Long actor, Long director, List<Long> lgdto, String str, String year, ProductTypes main) {
-	Query q = em.createQuery("select distinct p From Product p join p.films f join f.genre g where g.name='Action' ", Product.class);
-	q.setFirstResult(100);
-	q.setMaxResults(100);
-	List<Product> lp = q.getResultList();
-	return lp;
-	/*List<Product> res = new ArrayList<>();
-	for (Product p : lp) {
-	    if (main.equals(ProductTypes.All) || (p.getFilms().size() == 1 && main.equals(ProductTypes.Main)) || (p.getFilms().size() > 1 && main.equals(ProductTypes.Pack))) {
-		boolean add = false;
-		for (Film f : p.getFilms()) {
-		    if (ManageEntitieFilm.filterFilm(f, actor, director, lgdto, str, year, em)) {
-			add = true;
-		    }
-		}
-		if (add) {
-		    res.add(p);
-		}
-	    }
-	}
-	return res;*/
-    }
-
     @Override
     public FilteredListProductsDto getFilteredProducts(Long actor, Long director, List<Long> lgdto, String str, String year, OrderTypes sort, Integer limit, Integer row, ProductTypes main) {
-	List<Product> lpdto = this.findProducts(actor, director, lgdto, str, year, main);
-	int size = lpdto.size();
-	if (sort.equals(OrderTypes.RAND)) {
-	    Collections.shuffle(lpdto);
-	} else if (!sort.equals(OrderTypes.NO)) {
-	    Collections.sort(lpdto, ManageEntitieProduct.getComparator(sort));
-	}
-	if (row == null || row < 0) {
+	String query = "select distinct p From Product p join p.films f join f.genre g join f.actors a join f.directors d ";
+	if (row == null) {
 	    row = 0;
 	}
-	if (limit == null || limit == 0) {
-	    limit = lpdto.size() - row;
+	if (limit == null) {
+	    limit = 100;
 	}
-	if (row + limit > lpdto.size()) {
-	    limit = lpdto.size() - row;
+	boolean where = false;
+	if (actor != null && !actor.equals(0L)) {
+	    query += " where  a.id=" + actor;
+	    where=true;
 	}
-
-	lpdto = lpdto.subList(row, row + limit);
+	if (director != null && !actor.equals(0L)) {
+	    if (where) {
+		query += " and ";
+	    } else {
+		query += " where ";
+		where=true;
+	    }
+	    query += " d.id" + director;
+	}
+	if (lgdto != null && !lgdto.isEmpty()) {
+	    if (where) {
+		query += " and (";
+		
+	    } else {
+		query += " where (";
+		where=true;
+	    }
+	    boolean first = true;
+	    for ( Long g : lgdto)
+	    {
+		if (first )
+		    first=false;
+		else
+		    query+=" or ";
+		query += " g.id="+g;
+	    }
+	    query+=" ) ";
+	}
+	if (str!=null && !str.equals("")  )
+	{
+	    	    if (where) {
+		query += " and (";
+		where=true;
+	    } else {
+		query += " where (";
+	    }
+		    query += "  f.title like '%"+str+"%' or p.name like '%"+str+"%' ) "; 
+	}
+	Query q = em.createQuery(query, Product.class);
+	q.setFirstResult(row);
+	q.setMaxResults(limit);
+	List<Product> lpdto = q.getResultList();
 	List<ProductDto> res = new ArrayList<>();
 	for (Product p : lpdto) {
 	    res.add(ProductDtoManager.getDto(p));
 	}
-	return new FilteredListProductsDto(res, size);
+	return new FilteredListProductsDto(res, 3500);
     }
 
     @Override
