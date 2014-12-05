9185cfd5e0a6442b64b1570f195345c11fe195be
Sébastien Berthier <sebastien.berthier@e.ujf-grenoble.fr>
2014-12-05 19:30:54 +0100

    bug retrouver mot de passe ...

--- HomeCinema-ejb/src/java/entities/UserRetrieve.java
+++ HomeCinema-ejb/src/java/entities/UserRetrieve.java
@@ -6,18 +6,12 @@
 package entities;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
 /**
