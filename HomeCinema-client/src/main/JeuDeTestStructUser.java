df0dc4b90ab8554486e26c7579efb2c5a62ff5f8
Sébastien Berthier <sebastien.berthier@e.ujf-grenoble.fr>
2014-12-05 12:55:56 +0100

    Merge origin/sprint1.5 into sprint1.5
    Conflicts:
    	HomeCinema-client/src/main/JeuDeTest2.java
    	HomeCinema-client/src/main/JeuDeTestRobin.java
    	HomeCinema-client/src/main/JeuDeTestStruct.java
    	HomeCinema-client/src/main/JeuDeTestStructActivation.java
    	HomeCinema-client/src/main/JeuDeTestStructUser.java
    	HomeCinema-war/nbproject/project.properties

--- HomeCinema-client/src/main/JeuDeTestStructUser.java
+++ HomeCinema-client/src/main/JeuDeTestStructUser.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONException;
 
 /**
