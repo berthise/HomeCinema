6708746fb287121b513080cd3b487c63d67e600f
toure <toure@toure-S550CM>
2014-12-16 01:00:04 +0100

    Gestion des conteneurs vides dans moncompte, ajout de la fermeture sur les alerts

--- HomeCinema-war/web/js/main.js
+++ HomeCinema-war/web/js/main.js
@@ -87,7 +87,9 @@ $(function () {
         return false;
     });
 
-    $("*[title]").tooltip()
+    $(".alert").append('<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>');
+
+    $("*[title]").tooltip();
 });
 
 var myLang;
